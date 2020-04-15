697f039ce6b84b65ba6a7b5d9c379b027aa11f13
Lizzie <lchan217@gmail.com>
2020-04-15 09:56:52 -0700

    add content to location page

--- src/components/LocationNow.js
+++ src/components/LocationNow.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import "../css/LocationNow.css";
+import Moment from "react-moment";
 
-const LocationNow = () => {
-  return <div>Location Now</div>;
-};
+class LocationNow extends Component {
+  constructor() {
+    super();
+    this.state = {
+      longitude: "",
+      latitude: "",
+      time: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://api.open-notify.org/iss-now.json")
+      .then((response) => response.json())
+      .then((response) =>
+        this.setState({
+          longitude: response.iss_position.longitude,
+          latitude: response.iss_position.latitude,
+          time: response.timestamp,
+        })
+      );
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Location Now</h2>
+        <p className='location-data'>
+          <div>Longitude: {this.state.longitude}</div>
+          <div>Latitude: {this.state.latitude}</div>
+          <div>
+            Time Loaded:{" "}
+            <Moment unix format='MM/DD/YY HH:mm'>
+              {this.state.time}
+            </Moment>
+          </div>
+        </p>
+      </div>
+    );
+  }
+}
 
 export default LocationNow;
