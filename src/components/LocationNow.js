76d9d2f842d76cc0dc0afacf177d6abb8725c4ad
Lizzie <lchan217@gmail.com>
2020-04-16 10:53:10 -0700

    add loader to location now

--- src/components/LocationNow.js
+++ src/components/LocationNow.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../css/LocationNow.css";
 import Moment from "react-moment";
+import { Dimmer, Loader } from "semantic-ui-react";
 
 class LocationNow extends Component {
   constructor() {
@@ -9,6 +10,7 @@ class LocationNow extends Component {
       longitude: "",
       latitude: "",
       time: "",
+      isLoading: true,
     };
   }
 
@@ -22,27 +24,38 @@ class LocationNow extends Component {
           longitude: response.iss_position.longitude,
           latitude: response.iss_position.latitude,
           time: response.timestamp,
+          isLoading: false,
         })
       );
   }
 
   render() {
-    return (
-      <div>
-        <h2>Current Location</h2>
-        <p className='location-data'>
-          Longitude: {this.state.longitude} <br />
-          Latitude: {this.state.latitude}
-          <br />
-          Time Loaded:{" "}
-          <Moment unix format='MM/DD/YY HH:mm'>
-            {this.state.time}
-          </Moment>
-          <br />
-          Unix Time Stamp: {this.state.time}
-        </p>
-      </div>
-    );
+    let data;
+    if (this.state.isLoading) {
+      data = (
+        <Dimmer active inverted>
+          <Loader inline='centered'>Loading</Loader>
+        </Dimmer>
+      );
+    } else {
+      data = (
+        <div>
+          <h2>Current Location</h2>
+          <p className='location-data'>
+            Longitude: {this.state.longitude} <br />
+            Latitude: {this.state.latitude}
+            <br />
+            Time Loaded:{" "}
+            <Moment unix format='MM/DD/YY HH:mm'>
+              {this.state.time}
+            </Moment>
+            <br />
+            Unix Time Stamp: {this.state.time}
+          </p>
+        </div>
+      );
+    }
+    return <div>{data}</div>;
   }
 }
 
